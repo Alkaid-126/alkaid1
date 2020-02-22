502627f5b9bac4dccf09e82aedcf8a07c89e780f
Alkaid <377360604@qq.com>
2020-02-22 16:35:31 +0800

    Initial commit

---
+++ src/main/java/com/alkaid/alkaid/AlkaidApplication.java
@@ -0,0 +1,13 @@
+package com.alkaid.alkaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkaidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkaidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alkaid/alkaid/AlkaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkaid.alkaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkaidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
